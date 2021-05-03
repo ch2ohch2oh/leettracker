01ea7891d9cdb31b9c20ebd96ebc1db7c6668c09
Dazhi Wang <ch2ohch2oh@gmail.com>
2021-05-03 18:45:59 -0400

    fix the submission time issue

--- react/src/components/UserSubmissionCard.js
+++ react/src/components/UserSubmissionCard.js
@@ -7,20 +7,22 @@ export default function UserSubmissionCard({ subs, showUsername }) {
   if (!subs) {
     return <Paper>Loading</Paper>;
   } else {
-    console.log("add ids:", subs);
+    // console.log("add ids:", subs);
     const len = subs.length || 0;
     for (let i = 0; i < len; i++) {
-      subs[i].id = subs[i].timestamp;
-      // Covert epoch to local datetime
-      console.log(subs[i].timestamp);
-      subs[i].timestamp = new Date(parseInt(subs[i].timestamp) * 1000);
+      subs[i].id = i;
+      // Covert epoch to local datetime. Use setUTCSeconds to avoid overflow.
+      let date = new Date(0);
+      date.setUTCSeconds(parseInt(subs[i].timestamp));
+      // Do not modify timestamp inplace. No idea why it does not work.
+      subs[i]["time"] = date.toLocaleString();
     }
 
     const columns = [
       { field: "title", headerName: "Title", width: 500 },
       { field: "lang", headerName: "Language", width: 150 },
       { field: "status", headerName: "Status", width: 200 },
-      { field: "timestamp", headerName: "Time", width: 200, type: "dateTime" },
+      { field: "time", headerName: "Time", width: 200 },
     ];
     if (showUsername) {
       columns.unshift({
@@ -31,7 +33,6 @@ export default function UserSubmissionCard({ subs, showUsername }) {
     }
     return (
       <Paper>
-        {console.log(subs)}
         <DataGrid rows={subs} columns={columns} pageSize={10} autoHeight />
       </Paper>
     );
